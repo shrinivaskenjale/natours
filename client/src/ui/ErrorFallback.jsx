7cf9f4a9afc154d0e923823afdfd9607ce0b6f9e
Shrinivas Kenjale <sskenjale22@gmail.com>
2023-08-09 06:04:50 +0530

    Style error page

--- client/src/ui/ErrorFallback.jsx
+++ client/src/ui/ErrorFallback.jsx
@@ -1,19 +1,13 @@
+import "./ErrorFallback.css";
 import { Link } from "react-router-dom";
 
 const ErrorFallback = () => {
   return (
-    <section className="main">
-      <div className="error">
-        <div className="error__title">
-          <h2 className="heading-secondary heading-secondary--error">
-            Uh oh! Something went wrong!
-          </h2>
-          <h2 className="error__emoji">😢 🤯</h2>
-        </div>
-        <div className="error__msg">
-          <Link to={-1}>Go back</Link>
-        </div>
-      </div>
+    <section className="main error-screen section-center">
+      <h2>Something went wrong!</h2>
+      <Link className="button" to={-1}>
+        Go back
+      </Link>
     </section>
   );
 };
